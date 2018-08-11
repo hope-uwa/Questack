eb73015f5657c325cc6c54548c6dc321405604d2
uwahope007 <uwahope007@gmail.com>
2018-08-11 14:55:56 +0100

    'feat(UI)-view-user-profile[Started-#159674102]'

--- UI/assets/js/modal.js
+++ UI/assets/js/modal.js
@@ -2,9 +2,9 @@ window.onload = function(){
     let signUpModal = document.getElementById('signUpModal');
     let loginModal = document.getElementById('loginModal');
     let signupBtn = document.getElementById("signupModalBtn");
-    var closeSpanx = document.getElementsByClassName("close")[0];
+    let closeSpanx = document.getElementsByClassName("close")[0];
     let signupClose = document.getElementById('s-close');
-    var loginBtn = document.getElementById("loginModalBtn");
+    let loginBtn = document.getElementById("loginModalBtn");
     // let mainSec = document.getElementById('main');
     let signupBtnX =document.getElementById('signupModalBtnX');
 
@@ -23,10 +23,10 @@ window.onload = function(){
         loginModal.style.display = "none";
         
     };
-    // mainSec.onclick= ()=>{
-    //     signUpModal.style.display="none";
-    //     loginModal.style.display = "none";
-    // }
+    mainSec.onclick= ()=>{
+        signUpModal.style.display="none";
+        loginModal.style.display = "none";
+    }
     
     signupBtnX.onclick = () => {
         loginModal.style.display = "none";
--- UI/assets/js/script.js
+++ UI/assets/js/script.js
@@ -1,16 +1,13 @@
-window.onload = function(){ 
- let questionDisplayBtn = document.getElementById('askQuestion');
- let questionBanner = document.getElementById('questionBanner');
- let cancelQuestion = document.getElementById('cancel-question');
- questionDisplayBtn.onclick =()=>{
-    
-     questionBanner.style.display="block";
-     questionDisplayBtn.style.display="none";
- }
- cancelQuestion.onclick =()=>{
-    questionBanner.style.display="none";
-    questionDisplayBtn.style.display="block";
 
- }
- 
+let questionDisplayBtn = document.getElementById('askQuestion');
+let questionBanner = document.getElementById('questionBanner');
+
+let askQuestion = () => {
+    questionBanner.style.display = "block";
+    questionDisplayBtn.style.display = "none";
+
+};
+let cancelQuestion = () => {
+    questionBanner.style.display = "none";
+    questionDisplayBtn.style.display = "block";
 }
