ddb4d3d0428edf1650cde9cdcb042bca8cd66f74
uwahope007 <uwahope007@gmail.com>
2018-08-13 13:55:50 +0100

    'chore-ui'

--- UI/assets/js/modal.js
+++ UI/assets/js/modal.js
@@ -5,31 +5,18 @@ window.onload = function(){
     let closeSpanx = document.getElementsByClassName("close")[0];
     let signupClose = document.getElementById('s-close');
     let loginBtn = document.getElementById("loginModalBtn");
-     let mainSec = document.getElementById('main');
     let signupBtnX =document.getElementById('signupModalBtnX');
 
     
-    signupBtn.onclick = ()=> {
-        signUpModal.style.display = "inline-block";
-    }
     
-    loginBtn.onclick = () => {
-        loginModal.style.display = "inline-block";
-    }
-    signupClose.onclick = () => {
-        signUpModal.style.display = "none";
-    }
-    closeSpanx.onclick = () => {
-        loginModal.style.display = "none";
-        
-    };
-    mainSec.onclick= ()=>{
-        signUpModal.style.display="none";
-        loginModal.style.display = "none";
-    }
-    
-    signupBtnX.onclick = () => {
-        loginModal.style.display = "none";
-        signUpModal.style.display = "inline-block";
-    }
  }
+ let loginModalBtn = () => {
+    loginModal.style.display = "inline-block";
+}
+let signupModalBtn = ()=> {
+    signUpModal.style.display = "inline-block";
+}
+let closeModal = () => {
+        signUpModal.style.display = "none";
+        loginModal.style.display = "none";
+    }
