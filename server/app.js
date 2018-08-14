6a275632a16088c47eb371310f1eb59f8e1b27f1
uwahope007 <uwahope007@gmail.com>
2018-08-14 13:15:18 +0100

    'chore:setup-data[Finished-#159743916]'

---
+++ server/app.js
@@ -0,0 +1,20 @@
+import express from 'express';
+
+import bodyParser from 'body-parser'
+
+import routes from './routes';
+
+const app = express()
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+const port = process.env.PORT || 4000;
+routes(app);
+
+
+app.listen(port, () => {
+
+//  console.log(`Server running on port ${port}`);
+});
+
+export default app;
---
+++ server/data/index.js
@@ -0,0 +1,4 @@
+import users from './users.json';
+import questions from './questions.json';
+import answers from './answers.json';
+import comments from './comments.json';
