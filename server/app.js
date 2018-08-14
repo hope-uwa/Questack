1a3b12a4513aba5aa2170eaabc4056f75f791d6f
uwahope007 <uwahope007@gmail.com>
2018-08-14 13:34:09 +0100

    'chore:server-setup[Finished-#159743814]'

--- server/app.js
+++ server/app.js
@@ -1,10 +1,10 @@
 import express from 'express';
 
-import bodyParser from 'body-parser'
+import bodyParser from 'body-parser';
 
 import routes from './routes';
 
-const app = express()
+const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
