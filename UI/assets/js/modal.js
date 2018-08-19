52dd37bcfcc748198a226c917f4c9a54d86f5474
uwahope007 <uwahope007@gmail.com>
2018-08-19 01:19:44 +0100

    'bug-fix'

--- UI/assets/js/modal.js
+++ UI/assets/js/modal.js
@@ -1,22 +1,23 @@
-window.onload = function(){ 
+window.onload = function () {
     let signUpModal = document.getElementById('signUpModal');
     let loginModal = document.getElementById('loginModal');
     let signupBtn = document.getElementById("signupModalBtn");
     let closeSpanx = document.getElementsByClassName("close")[0];
     let signupClose = document.getElementById('s-close');
     let loginBtn = document.getElementById("loginModalBtn");
-    let signupBtnX =document.getElementById('signupModalBtnX');
+    let signupBtnX = document.getElementById('signupModalBtnX');
 
-    
-    
- }
- let loginModalBtn = () => {
+
+
+}
+let loginModalBtn = () => {
     loginModal.style.display = "inline-block";
 }
-let signupModalBtn = ()=> {
+let signupModalBtn = () => {
     signUpModal.style.display = "inline-block";
+    loginModal.style.display = "none";
 }
 let closeModal = () => {
-        signUpModal.style.display = "none";
-        loginModal.style.display = "none";
-    }
+    signUpModal.style.display = "none";
+    loginModal.style.display = "none";
+}
