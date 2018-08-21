78c9c95656d920128bbcbdf0621c8f56794af52e
uwahope007 <uwahope007@gmail.com>
2018-08-21 03:59:01 +0100

    feat(UI): implement dropdown for mobile view

--- UI/assets/js/script.js
+++ UI/assets/js/script.js
@@ -2,6 +2,26 @@
 let questionDisplayBtn = document.getElementById('askQuestion');
 let questionBanner = document.getElementById('questionBanner');
 
+
+let dropDownMenu = () =>{
+    document.getElementById('dropdown-menu').classList.toggle("show");
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropDownBtn')) {
+  
+      let dropdowns = document.getElementsByClassName("profile__actions");
+      let i;
+      for (i = 0; i < dropdowns.length; i++) {
+        let openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+
 let askQuestion = () => {
     questionBanner.style.display = "block";
     questionDisplayBtn.style.display = "none";
@@ -11,4 +31,5 @@ let cancelQuestion = () => {
     questionBanner.style.display = "none";
     questionDisplayBtn.style.display = "block";
 }
+
 
