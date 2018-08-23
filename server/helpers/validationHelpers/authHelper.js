744e722e9775f089d312e27ec8a2893efef9d7b2
uwahope007 <uwahope007@gmail.com>
2018-08-23 17:06:18 +0100

    feat(Validation): Validation helper for controllers [Started-#159947675]

---
+++ server/helpers/validationHelpers/authHelper.js
@@ -0,0 +1,79 @@
+import {
+  check, param, header, validationResult
+} from 'express-validator/check';
+
+const trimWhiteSpaces = (input) => {
+  const trimedString = input.replace(/^\s+|\s+$/g, '');
+  return trimedString;
+};
+
+
+const validateAuth = {
+
+  login: [
+    check('email', 'email is required')
+      .exists(),
+    check('email', 'please, enter a valid email')
+      .isEmail(),
+    check('password', 'password is required')
+      .exists()
+  ],
+  signUp: [
+    check('Username', 'Username is required and should be a minimum of 4 characters')
+      .isLength({ min: 4 })
+      .custom((value) => {
+        if (trimWhiteSpaces(value).length < 3) {
+          throw new Error('First name should be a minimum of 3 characters');
+        } else {
+          return value;
+        }
+      }),
+    check('email', 'email is required')
+      .exists(),
+    check('email', 'please, enter a valid email')
+      .isEmail(),
+    check('password', 'password should be a minimum of 6 chracters')
+      .isLength({ min: 6 })
+      .custom((value, { req }) => {
+        if (trimWhiteSpaces(value).length < 6) {
+          throw new Error('First name should be a minimum of 6 characters');
+        }
+        if (value !== req.body.confirmPassword) {
+          throw new Error("Password doesn't match");
+        } else {
+          return value;
+        }
+      })
+  ],
+
+  postQuestion: [
+    header('token', 'Token is required in header').exists(),
+    check('questionTitle', 'Question title is required')
+      .exists(),
+    check('questionBody', 'Question Body is required')
+      .exists(),
+    check('questionTitle', 'Question title can not be empty').isEmpty(),
+    check('questionBody', 'Question body can not be empty').isEmpty()
+
+  ],
+  getQuestion: [
+    header('token', 'Token is required in header').exists(),
+    param('questionId', 'Id must be integer').isInt()
+  ],
+  postAnswer: [
+    header('token', 'Token is required in header').exists(),
+    check('answerBody', 'Answer Body is required')
+      .exists(),
+    check('answerBody', 'Answer body can not be empty').isEmpty(),
+    param('questionId', 'Question ID must be an integer').isInt()
+
+  ],
+  getAnswer: [
+    header('token', 'Token is required in header').exists(),
+    param('questionId', 'Id must be integer').isInt(),
+    param('answerId', 'Id must be integer').isInt()
+  ]
+
+};
+
+export default validateAuth;
