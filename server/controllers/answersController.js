c6e18383fc181076356b6dd8f68319024104e3e4
uwahope007 <uwahope007@gmail.com>
2018-08-24 14:10:10 +0100

    bug: modify dummycontrollers [Fixed-#160017859]

--- server/controllers/answersController.js
+++ server/controllers/answersController.js
@@ -24,7 +24,7 @@ class AnswerController {
     const questionId = req.params.questionId;
     const userId = req.userId;
     const { answerBody } = req.body;
-    
+
     const createdAt = moment().format('YYYY-MM-DD');
     const getQuestionQuery = `SELECT * FROM questions WHERE question_id = '${questionId}'`;
 
@@ -58,54 +58,9 @@ class AnswerController {
            * @returns {(function|object)} Function next() or JSON object
            */
 
-  static getAnswers(req, res) {
-    const questionId = AnswerController.questionId(req);
-    const allAnswers = data.answers;
-
-    const findAnswer = allAnswers.filter(ans => ans.questionId == parseInt(questionId, 10));
-
-    if (findAnswer.length !== 0) {
-      return res.status(200).json({ answer: findAnswer })
-    }
-    return res.status(200).json({ answer: 'There is no answer given yet' })
-
-  }
-
-  /**
-       * Returns a message
-       * @method AddPreferredAnswer
-       * @memberof AnswerController
-       * @param {object} req
-       * @param {object} res
-       * @returns {(function|object)} Function next() or JSON object
-       */
-
-  static AddPreferredAnswer(req, res) {
-    const qid = AnswerController.questionId(req);
-    const aid = AnswerController.answerId(req);
-    const preferredAnswer = data.preferredAnswers;
-    const id = qid;
-
-    const findOption = preferredAnswer.findIndex(answer => answer.questionId === parseInt(id, 10))
-    if (findOption === -1) {
-      return res.status(404).json({ message: 'The question is not found' })
-    }
-
-    preferredAnswer[findOption].answerId = aid;
-    return res.status(201).json({ message: 'The preferred Answer has been choosen' });
-
-  }
 
 
-  static questionId(req) {
-    return req.params.questionId;
-  }
 
-  static answerId(req) {
-    return req.params.answerId;
-  }
-
-  
 
 }
 
