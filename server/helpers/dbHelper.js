dbaee696a387c006e4ba348e4d3a75d37a6d6e37
uwaelpis <42223360+uwaelpis@users.noreply.github.com>
2018-09-10 18:12:40 +0100

    Update dbHelper.js

--- server/helpers/dbHelper.js
+++ server/helpers/dbHelper.js
@@ -1,6 +1,6 @@
 import { Pool } from 'pg';
 
-const connection = process.env.DATABASE_URL || 'localhost://postgres:uwahope007@localhost:5432/stackoverflowtest';
+const connection = process.env.DATABASE_URL || process.env.TEST_DB_URL;
 
 const pool = (process.env.NODE_ENV === 'test') ? new Pool({ connectionString: process.env.TEST_DATABASE_URL }) : new Pool({ connectionString: connection });
 
