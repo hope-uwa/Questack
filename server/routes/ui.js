142f754ee2492f664a272a14196d70ea30bf2b3d
uwaelpis <42223360+uwaelpis@users.noreply.github.com>
2018-09-22 03:19:32 +0100

    Update ui.js

--- server/routes/ui.js
+++ server/routes/ui.js
@@ -3,11 +3,11 @@ import { Router } from 'express';
 const uiRoutes = Router();
 const root = './ui';
 
-uiRoutes.get('/', (req, res) => {
+uiRoutes.get('/index', (req, res) => {
   res.sendFile('index.html', { root })
 });
 uiRoutes.get('/prof',(req, res) => {
-  res.sendFile(path.join(__dirname + './ui/profile.html'));
+  res.sendFile(path.join(__dirname + '/ui/index.html'));
 });
 uiRoutes.get('/profile', (req, res) => {
   res.sendFile('auth/user-profile.html', { root })
