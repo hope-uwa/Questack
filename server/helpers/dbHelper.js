e854d989e131ef4b7548a0255a8b6bd5158d642a
uwahope007 <uwahope007@gmail.com>
2018-09-22 01:24:29 +0100

    chore: refactor

--- server/helpers/dbHelper.js
+++ server/helpers/dbHelper.js
@@ -1,6 +1,6 @@
 import { Pool } from 'pg';
 
-const connection = process.env.DATABASE_URL || 'postgres://xphjffwkceqqsr:8b60de777bf6a9a4ee92c989d614ce66dbe7b1ac656854f4d3fee157a69b33a9@ec2-54-83-51-78.compute-1.amazonaws.com:5432/ddfvvcckekgvda?ssl=true';
+const connection = process.env.DATABASE_URL;
 
 const pool = (process.env.NODE_ENV === 'test') ? new Pool({ connectionString: process.env.TEST_DATABASE_URL }) : new Pool({ connectionString: connection });
 
